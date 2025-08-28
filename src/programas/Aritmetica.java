1d6401b443abebebcd6c18eee8579f518d6013ad
Arnold Patu <Arnold Patu@192.168.0.8>
2025-08-27 22:34:00 -0500

    carga inicial

---
+++ src/programas/Aritmetica.java
@@ -0,0 +1,24 @@
+package programas;
+/**
+ *
+ * @author Arnold Paturri
+ */
+import java.util.Scanner;
+public class Aritmetica {
+    public static void main(String[] args){
+        //variables
+        double num1,num2,suma,promedio;
+        Scanner lectura=new Scanner (System.in);
+        //entrada
+        System.out.print("Ingresar numero 1: ");
+        num1=lectura.nextDouble();
+        System.out.print("Ingresar numero 2: ");
+        num2=lectura.nextDouble();
+        //operacion
+        suma=num1+num2;
+        promedio=suma/2;
+        
+       //salida
+       System.out.println("El promedio es "+promedio);
+    }
+}
